d199fd1a9c7c45c3c9d3fc180a61f30db0f5749d
Leonid Petrov <lpetrov@alfabank.ru>
2023-07-19 04:31:48 +0400

    serv

--- client/src/services/cities.service.ts
+++ client/src/services/cities.service.ts
@@ -1,8 +1,6 @@
-const apiHost = process.env.REACT_APP_API_HOST;
-
 export class CitiesService {
   public async getAllCities(): Promise<any> {
-    const response = await fetch(`${apiHost}/api/city`);
+    const response = await fetch(`/api/city`);
     return await response.json();
   }
 }
--- client/src/services/events.service.ts
+++ client/src/services/events.service.ts
@@ -1,9 +1,8 @@
 import { EventEntity } from '../types';
-const apiHost = process.env.REACT_APP_API_HOST;
 
 export class EventsService {
   public async getAllEventInCity(city_id: string = ''): Promise<any> {
-    const response = await fetch(`${apiHost}/api/events-in-city/${city_id}`);
+    const response = await fetch(`/api/events-in-city/${city_id}`);
     const data = await response.json();
     return data.map((event: EventEntity) => ({
       ...event,
@@ -12,7 +11,7 @@ export class EventsService {
   }
 
   public async getEvent(event_id: string): Promise<EventEntity> {
-    const response = await fetch(`${apiHost}/api/event/${event_id}`);
+    const response = await fetch(`/api/event/${event_id}`);
     return await response.json();
   }
 }
